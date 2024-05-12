3197ac3b1988b3dd1f943042811c277f7ebdf214
greatdomino <domino.net@gmail.com>
2024-05-12 16:22:52 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-// 填充之前保存的凭据和存储桶名称
+// Populate previously saved credentials and storage bucket names
 function fillSavedCredentials() {
     chrome.storage.sync.get(['endpointUrl', 'accessKeyId', 'secretAccessKey', 'bucketName'], (data) => {
         console.log('Saved credentials:', data);
@@ -9,7 +9,7 @@ function fillSavedCredentials() {
     });
 }
 
-// 在弹出窗口打开时填充之前保存的凭据和存储桶名称
+// Populate previously saved credentials and bucket names when the popup window opens
 document.addEventListener('DOMContentLoaded', fillSavedCredentials);
 
 document.getElementById('save-btn').addEventListener('click', () => {
@@ -18,25 +18,25 @@ document.getElementById('save-btn').addEventListener('click', () => {
     const secretAccessKey = document.getElementById('secret-access-key').value;
     const bucketName = document.getElementById('bucket-name').value; // 获取存储桶名称
 
-    // 保存凭据和存储桶名称到浏览器存储
+    // Saving credentials and bucket names to browser storage
     chrome.storage.sync.set({
         endpointUrl,
         accessKeyId,
         secretAccessKey,
-        bucketName // 保存存储桶名称
+        bucketName // Save bucket name
     }, () => {
         console.log('Credentials and bucket name saved');
-        // 显示成功提示信息
+        // Show success message
         document.getElementById('success-msg').style.display = 'block';
 
-        // 2秒后隐藏提示信息
+        // Hide alert message after 2 seconds
         setTimeout(() => {
             document.getElementById('success-msg').style.display = 'none';
         }, 2000);
     });
 });
 
-// 在弹出窗口关闭时重新加载背景页面
+// Reload background page when popup window closes
 window.addEventListener('unload', () => {
     chrome.runtime.reload();
 });
